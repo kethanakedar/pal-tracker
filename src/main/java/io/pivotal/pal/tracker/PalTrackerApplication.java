5289c2c284b83e6e4b5c960e8b8e86a1d66d5909
kethanakedar <kedareswar.gopalam@cognizant.com>
2020-05-29 09:28:35 -0400

    updated

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,10 +3,23 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+
+
+
+import javax.sql.DataSource;
 @SpringBootApplication
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+    @Bean
+
+    TimeEntryRepository timeEntryRepository() {
+
+        return new InMemoryTimeEntryRepository();
+
     }
 }
